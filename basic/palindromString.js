02cbd3a21085085c09a356ca5b1850772935847a
kriti kumari <123kritikumari@gmail.com>
2026-03-25 09:07:27 +0530

    optimized

--- basic/palindromString.js
+++ basic/palindromString.js
@@ -1,4 +1,5 @@
-const palindromFun = (s) =>{
+// Time Complicity : o(n)
+const palindromFun = (s) =>{  
 
     let rev = s.split("").reverse().join("")
     return s === rev
@@ -8,3 +9,30 @@ const palindromFun = (s) =>{
 let str = "MOM"
 console.log(palindromFun(str))
 
+// Time Complicity : o(n)
+const palindrom = (s) => {
+    let rev = ""
+    for(let i=s.length -1; i>=0; i--){
+        rev +=s[i]
+    }
+    return rev
+}
+let str1 = "MOM"
+console.log(palindrom(str1))
+
+// Time Complicity : o(1)
+
+const palindromBest = (s) => {
+    let Left = 0
+    let Right = s.length-1
+
+    while(Left<Right){
+        if(s[Left] !== s[Right]) return false
+        Left++
+        Right--
+    }
+
+    return true
+}
+let str2 = "MOM"
+console.log(palindromBest(str2))
